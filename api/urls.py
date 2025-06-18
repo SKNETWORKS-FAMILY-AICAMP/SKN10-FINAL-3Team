7355b942e10d10615e9612b3e64e10f1f53cd6d7
seunggi-coding <moonsys010103@gmail.com>
2025-06-18 17:50:35 +0900

    api 앱에 있는 필요없는 url과 view 함수 제거

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,12 @@
 # user/urls.py
 from django.urls import path
 from .views import (
-    RecommendTeamAPIView, EventCreateAPIView, 
-    EventListByOrgAPIView
+    RecommendTeamAPIView, EventCreateAPIView
 )
 
 # API 엔드포인트 URL 구성
 urlpatterns = [
-    path('recommend/', RecommendTeamAPIView.as_view(), name='api-recommend'),       # 프로필 API
+    path('recommend/', RecommendTeamAPIView.as_view(), name='api-recommend'),       # 추천 API
     path('event/create/', EventCreateAPIView.as_view(), name='api-event-create'),   # 사건 생성 API
-    path('event/by-org/', EventListByOrgAPIView.as_view(), name='api-event-by-org'),# 사건 목록 조회 API
 
 ]
